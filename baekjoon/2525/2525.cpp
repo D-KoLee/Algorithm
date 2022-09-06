156b309badfc6bbd6d1d0b5afd6747831b748352
D-KoLee <by07226@naver.com>
2022-09-06 13:27:09 +0900

    2525 solved

---
+++ baekjoon/2525/2525.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int h, m, t;
+    cin >> h >> m >> t;
+    m += t;
+    h += m / 60;
+    m %= 60;
+    h %= 24;
+    cout << h << " " << m;
+    return 0;
+}
