cbe4fedf7269896af2e071af04c90e9424675404
D-KoLee <by07226@naver.com>
2022-09-13 23:12:23 +0900

    10872 solved

--- baekjoon/10872/10872.cpp
+++ baekjoon/10872/10872.cpp
@@ -3,13 +3,11 @@ using namespace std;
 
 int fibo(int n)
 {
-    if (n == 0)
-        return 0;
-    else if (n <= 2)
-    {
-        return 1;
+    if(n > 1) {
+        return n * fibo(n - 1);
     }
-    return (fibo(n - 1) + fibo(n - 2));
+    else
+        return 1;
 }
 
 int main()
